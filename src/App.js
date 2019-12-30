072899272ad3658b80e1f5bc6d8dbbbaec5f3b3a
hj921 <comdat88@gmail.com>
2019-12-30 18:30:23 +0900

    React setState

--- src/App.js
+++ src/App.js
@@ -1,68 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "https://cdn.crowdpic.net/detail-thumb/thumb_d_CDC14868821FF3F20C77BC8BC15E6355.jpg",
-    rating: 4
-  },
-  {
-    id: 2,
-    name: "Kimbab",
-    image:
-      "https://s3.ap-northeast-2.amazonaws.com/event-localnaeil/FileData/Article/201806/a8d34579-3939-498b-9254-6e3cfac4bfda.jpg",
-    rating: 4
-  },
-  {
-    id: 3,
-    name: "Ramen",
-    image:
-      "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/4IqY/image/pWLCAnbTwLA3G7TU8BkrCGurl4A.jpg",
-    rating: 5
-  },
-  {
-    id: 4,
-    name: "Americano",
-    image:
-      "https://img.insight.co.kr/static/2018/01/08/700/9582pp72zb162gryb9t4.jpg",
-    rating: "5"
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({count: current.count + 1}));
+  };
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}));
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
   }
-]; // food의 object의 배열
-
-// function renderFood(dish) {
-//   // console.log(dish);
-//   return <Food name={dish.name} picture={dish.image} />
-// }
-
-function Food({name, picture, rating}) {
-  // console.log(props.fav);
-  return <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}/>
-    </div>
-}
-
-Food.propTypes = {
-  // 내가 얻고싶은 props에대한 설명
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
-      ))}
-      {/* {console.log(foodILike.map(renderFood))}
-      {foodILike.map(renderFood)} */}
-    </div>
-  );
 }
 
 export default App;
